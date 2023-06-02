0abbbdf7686c6fa36e5179d6fac035984f2c161d
Domenique <domogenesis77@gmail.com>
2023-06-01 23:10:55 -0500

    ahhhhh

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+
+const newArray = [];
+function writeCards(name){
+  for  (let i = 0; i < name.length;i++){ 
+    newArray.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+  };
+  ;
+  return newArray};
+
+
+  const n = 10;
+  
+  
+  
+  
+  var i= "10";
+
+function countDown(i)
+
+{
+
+    while( i >= 0)
+
+    {
+        console.log(i);
+        i--;
+    }
+}
+countDown();
+
